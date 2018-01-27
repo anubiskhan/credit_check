4c81b23e5cffb679b89bae5281f5626dfc1f6183
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-01-26 22:46:49 -0700

    Broke my code,pretty sure I need to rework all my methods. Will be a fun Sat

--- credit_check.rb
+++ credit_check.rb
@@ -1,38 +1,54 @@
-card_number = "4929735477250543"
+  card_number = "4929735477250543"
 
-valid = false
+  valid = false
 
-# Your Luhn Algorithm Here
-def LuhnCheck ( card_number )
-  card_number = card_number.split("")
-  card_number = card_number.reverse
-  card_number = card_number.map(&:to_i)
-  card_number = card_number.map.with_index { |digit, index|
-    if index.odd?
-      digit = digit*2
-    else
-      digit = digit
-    end
-  }
-  card_number = card_number.map { |digit|
-    if digit > 9
-      digit = digit - 9
-    else
-      digit = digit
-    end
-  }
-  card_sum = card_number.inject(0){|sum, digit| sum + digit }
-  puts card_number
-  card_sum
-end
-cc_sum = LuhnCheck("4929735477250543")
-puts cc_sum
-# Output
-## If it is valid, print "The number is valid!"
+  def make_cc_number_an_array(card_number)
+    @card_number2 = card_number.split("")
+    # reverse_cc_number_array(card_number2)
+  end
+
+  def reverse_cc_number_array(card_number2)
+    @card_number3 = @card_number2.reverse
+    # makes_cc_number_array_integers(card_number3)
+  end
+
+  def makes_cc_number_array_integers(card_number3)
+    @card_number4 = @card_number3.map(&:to_i)
+    # doubles_appropriate_cc_numbers(card_number4)
+  end
+
+  def doubles_appropriate_cc_numbers(card_number4)
+    @card_number5= @card_number4.map.with_index { |digit, index|
+      if index.odd?
+        digit = digit*2
+      else
+        digit = digit
+      end
+    }
+    # reduces_appropriate_cc_numbers(card_number5)
+  end
+
+  def reduces_appropriate_cc_numbers(card_number5)
+    @card_number6 = @card_number5.map { |digit|
+      if digit > 9
+        digit = digit - 9
+      else
+        digit = digit
+      end
+    }
+    # sums_cc_number_array(card_number6)
+  end
+
+  def sums_cc_number_array(card_number6)
+    card_sum = @card_number6.inject(0){|sum, digit| sum + digit }
+    puts card_number
+    card_sum
+  end
+  cc_sum = sums_cc_number_array("4929735477250543")
+  puts cc_sum
 
 if cc_sum % 10 == 0
   puts "The number is valid!"
 else
   puts "The number is invalid"
 end
-## If it is invalid, print "The number is invalid!"
--- test_credit_check.rb
+++ test_credit_check.rb
@@ -6,5 +6,8 @@ require './credit_check.rb'
 
 class CreditCheckTest < Minitest::Test
   def test_credit_card_number_validity
+
+    assert_equal 80, luhn_check("4929735477250543")
+
   end
 end
